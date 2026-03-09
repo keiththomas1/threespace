0e2d02703e8e9c3c98b339bd111550ba174ca6dd
Keith Thomas <thedevkeith@gmail.com>
2026-03-09 14:19:45 -0500

    Updated action to reflect demo workflow

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,10 +1,12 @@
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['threespace'],
   experimental: { esmExternals: true, externalDir: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
